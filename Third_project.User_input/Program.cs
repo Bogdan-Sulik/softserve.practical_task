e301c0465caf2f043a7bb16437396e1bf78d841f
Bohdan <91536535+Bogdan-Sulik@users.noreply.github.com>
2023-10-27 09:59:41 +0300

    Initial_commit

---
+++ Third_project.User_input/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Third_project.User_input;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Is bear sleeping? Press ‘{UserInputConstants.PositiveResponse}’ if he is:");
+
+        bool isPositiveResponse = string.Equals(Console.ReadLine(),
+            UserInputConstants.PositiveResponse, StringComparison.OrdinalIgnoreCase);
+
+        string bearImage = isPositiveResponse
+            ? ImageAsStringConstants.BearWithOpenedEyes 
+            : ImageAsStringConstants.BearWithClosedEyes;
+
+        Console.WriteLine(bearImage);
+    }
+}
